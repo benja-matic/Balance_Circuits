dae20ab546fad51843db594b19f3a9c94d14c35b
benja-matic <benjapaulcohen@gmail.com>
2018-05-12 10:55:50 -0400

    fixed input characterization by summing over columns instead of finding non-zeros. This takes into account tupled connections

--- Local_Circuits/run_2x2_fast.jl
+++ Local_Circuits/run_2x2_fast.jl
@@ -4,8 +4,8 @@ include("2x2_fast.jl")
 srand(4321)
 
 Aee = 12.5
-Aei = 20.
 Aie = 50.
+Aei = 20.
 Aii = 50.
 
 N = 5000
@@ -22,7 +22,7 @@ Ni2 = round(Int64, Ni/2)
 fe1 = 3.
 fi1 = fe1 - .15
 
-fe2 = fe1 + .5
+fe2 = fe1 #+ .5
 fi2 = fe2 - .15
 
 vth = 20
@@ -59,7 +59,8 @@ er2 = setdiff(Set(1:length(re)), er1);
 MER1 = length(er1)/(Ne2*rt);
 MER2 = length(er2)/(Ne2*rt);
 
-
+figure(1)
+plot(t,r,".")
 
 ###################
 #IE means E to I, 12 means from 2 to 1
@@ -82,9 +83,13 @@ function weights_lookup(i, raster, j_inds)
   kx = find(raster .== i)
   if length(ks) > 0
     for j = 1:4
-      wxr = find(W[compartments[j]:compartments[j] + Ne2, i])
-      A = W[compartments[j]:compartments[j] + Ne2, i][wxr[1]]
-      output = A * tau_s * length(wxr) * length(kx)#integral over synaptic waveform * number_outputs * number of spikes *
+      # wxr = find(W[compartments[j]:compartments[j] + Ne2, i])
+      # A = W[compartments[j]:compartments[j] + Ne2, i][wxr[1]]
+      # output = A * tau_s * length(wxr) * length(kx)#integral over synaptic waveform * number_outputs * number of spikes *
+      ### fails to account for tupled connections
+      ### should really be sum over column compartment * tau_s * number of spikes
+      ws = sum(W[compartments[j]:compartments[j] + Ne2, i])
+      output = ws * tau_s * length(kx)
       s_inputs[j_inds[j]] += output
     end
   end
@@ -109,15 +114,131 @@ end
 
 normz = Ne2 * runtime
 
-input_strings = ["EE11", "EE22", "EE21", "EE12", "IE11", "IE22", "IE21", "IE12", "EI11", "EI22", "EI21", "EI12", "II11", "II22", "II21", "II11",]
+input_strings = ["EE11", "EE22", "EE21", "EE12", "IE11", "IE22", "IE21", "IE12", "EI11", "EI22", "EI21", "EI12", "II11", "II22", "II21", "II12",]
 
 s_inf  = s_inputs ./ normz
-
+figure(2)
 bar(1:16, s_inf, tick_label = input_strings)
 xticks(rotation = 45, fontsize = 24)
 yticks(fontsize = 24)
 title("Characterizing Inputs in Brent's Network", fontsize = 36)
 
 #
-s_inf[2] + s_inf[3] + s_inf[10] + s_inf[11] + fe2
-s_inf[1] + s_inf[4] + s_inf[9] + s_inf[12] + fe1
+s_inf[2] + s_inf[3] + s_inf[10] + s_inf[11] + fe2 #input to E pool 2 (dominant)
+s_inf[1] + s_inf[4] + s_inf[9] + s_inf[12] + fe1 #input to E pool 1 (suppressed)
+
+s_inf[6] + s_inf[7] + s_inf[14] + s_inf[15] + fi2#input to I pool 2 (dominant)
+s_inf[5] + s_inf[8] + s_inf[13] + s_inf[16] + fi1#input to I pool 1 (suppressed)
+
+
+###checking for bugs
+#first issue is that you can have repeat indices
+#to resolve this we can compute sums over every row and column and compare to what I think we should get
+Jee = Aee/ks
+Jei = -Aei/ks
+Jie = Aie/ks
+Jii = -Aii/ks
+
+e_row_t = Jee*k + Jei*k
+i_row_t = Jie*k + Jii*k
+# e_col_t =
+
+# c_out = zeros(N);
+# c_outx = zeros(N);
+# c_in = zeros(N);
+# c_inx = zeros(N);
+#
+# for i = 1:N
+#   kx = sum(W[:,i])
+#   c_out[i] = kx
+#   kx2 = sum(W[i,:])
+#   c_outx[i] = length(find(W[:,i]))
+#   c_in[i] = kx2
+#   c_inx[i] = length(find(W[i,:]))
+# end
+# figure(3)
+# plot(c_in)
+# axhline(e_row_t, linestyle = "dashed")
+# axhline(i_row_t, linestyle = "dashed")
+#that's a good lookin' check
+
+#plot(c_out)
+#outputs are not constrained, so we expect some heterogeneity
+#Each time we decide who supplies inputs, we choose a neuron with probability k2/(Ne2=Ni2) = k/(Ne=Ni)
+#This experiment is repeated N times, thus we expect each neuron to have N*k/Ne outputs = 2k
+#plot(c_outx) #looks like the number of UNIQUE outgoing connections is a bit less than 2k...approximately correct
+#is it really 2k - (N*2k/N^2) - (N*2k/N^3) - ...? whenever you get doubled, someone else gets left out
+# #we know past N, the terms are virtually vanishing, just subtract out N^2 terms
+# p0 = 2k/N
+# p1 = 2k/N^2
+# #probability of being chosen is 2k/N for each row, so probability of not being chosen is 1 - 2k/n ~ .68
+# figure(4)
+# plot(sort(c_inx), sort(c_outx))
+# plot([1200,1600], [1200, 1600], "r")
+#this plot seems to say that there are a higher number of unique outgoing connections than incoming connections
+
+
+##try checking some sums over Ne2 sized pieces of columns
+ees1 = zeros(Ne2);
+ees2 = zeros(Ne2);
+eis1 = zeros(Ni2);
+eis2 = zeros(Ni2);
+eis3 = zeros(Ni2);#just spot check pool 2 E neurons
+eis4 = zeros(Ni2);
+ies1 = zeros(Ne2);
+ies2 = zeros(Ne2);
+iis1 = zeros(Ni2);
+iis2 = zeros(Ni2);
+
+for i = 1:Ne2
+  e1 = sum(W[1:Ne2, i])
+  e2 = sum(W[1:Ne2, i+Ne2])
+  ees1[i] = e1
+  ees2[i] = e2
+end
+
+for i = 1:Ne2
+  e1 = sum(W[1:Ne2, i+Ne])
+  e2 = sum(W[1:Ne2, i+Ne+Ni2])
+  eis1[i] = e1
+  eis2[i] = e2
+end
+
+for i = 1:Ne2
+  e1 = sum(W[Ne+1:Ne+Ni2, i])
+  e2 = sum(W[Ne+1:Ne+Ni2, i+Ni2])
+  ies1[i] = e1
+  ies2[i] = e2
+end
+
+for i = 1:Ne2
+  e1 = sum(W[Ne+1:Ne+Ni2, i+Ne])
+  e2 = sum(W[Ne+1:Ne+Ni2, i+Ne+Ni2])
+  iis1[i] = e1
+  iis2[i] = e2
+end
+
+for i = 1:Ne2
+  e1 = sum(W[Ne2+1:Ne, i+Ne])
+  e2 = sum(W[Ne2+1:Ne, i+Ne+Ni2])
+  eis3[i] = e1
+  eis4[i] = e2
+end
+
+#verified matrix is not biased, issue must be in my measures
+
+#
+
+
+#c_in should have 0 heterogeneity, should just be 2k every time
+
+# Wx = zeros(N,N);
+# e_inds = rand(1:Ne2, k2);
+# i_inds = rand(Ne+1:Ne+Ni2, k2);
+#
+# for j in eachindex(e_inds)
+#   Wx[1, e_inds[j]] += 1
+#   Wx[1, e_inds[j] + Ne2] += 1
+#   Wx[1, i_inds[j]] += 1
+#   Wx[1, i_inds[j] + Ni2] += 1
+# end
